9b2714e5386e2bb6b2d312f8957c2115aab898e1
pravin-git <pravinpawar@gmail.com>
2017-04-25 14:06:11 +0530

    Initial Draft

---
+++ Acme.Web.Client/App_Start/FilterConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Acme.Web.Client
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            var authorizeAttribute = new AuthorizeAttribute()
+            {
+                Roles = "infovisionlabs\\sigmaflow"
+            };
+            filters.Add(authorizeAttribute);
+        }
+    }
+}
---
+++ Acme.Web.Client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Acme.Web.Client.MvcApplication" Language="C#" %>
